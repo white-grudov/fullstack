00e019062a35147431a676c018745bdda5ff63b7
Daniel Belogrudov <danekbel.1@gmail.com>
2023-07-02 16:56:42 +0300

    Fix

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.createRoot(rootElement).render(<App />);
-
 const PORT = process.env.PORT || 3001;
+
+ReactDOM.createRoot(rootElement).render(<App />);
 
 console.log(`Server running on port ${PORT}`);
